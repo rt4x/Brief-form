2d86c3d9580d11df27919b4a7c8720ba84dfdb91
Dmytro Kalinovskyi <deeeperxd@gmail.com>
2026-04-26 19:44:44 +0200

    Add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "Brief Form",
+};
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 import { AppHeader } from "@/components/app-header";
